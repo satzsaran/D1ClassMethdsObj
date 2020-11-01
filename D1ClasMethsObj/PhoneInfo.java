c11c9d437e6694df0d721819a39eea69bb6c5282
satzsaran <73731493+satzsaran@users.noreply.github.com>
2020-11-01 08:01:33 -0500

    Add files via upload

---
+++ D1ClasMethsObj/PhoneInfo.java
@@ -0,0 +1,34 @@
+package org.emp;
+
+public class PhoneInfo {
+private void phoneName() {
+	// TODO Auto-generated method stub
+System.out.println("apple");
+}
+private void phoneImeiNum() {
+	// TODO Auto-generated method stub
+System.out.println("123654789");
+}
+private void phoneCam() {
+	// TODO Auto-generated method stub
+System.out.println("12mp");
+}
+private void phoneStorage() {
+	// TODO Auto-generated method stub
+System.out.println("128gb");
+}
+private void phoneOs() {
+	// TODO Auto-generated method stub
+System.out.println("mac");
+}
+public static void main(String[] args) {
+	
+PhoneInfo obj = new PhoneInfo();
+	
+	obj.phoneName();
+	obj.phoneImeiNum();
+	obj.phoneCam();
+	obj.phoneStorage();
+	obj.phoneOs();
+}
+}
---
+++ D1ClasMethsObj/stateDetails.java
@@ -0,0 +1,21 @@
+package org.emp;
+
+public class stateDetails {
+private void southIndia() {
+	// TODO Auto-generated method stub
+System.out.println("tamil english");
+}
+private void northIndia() {
+	// TODO Auto-generated method stub
+System.out.println("hindi english");
+}
+public static void main(String[] args) {
+	stateDetails obj= new stateDetails();
+	obj.southIndia();
+	obj.northIndia();
+	LanguageInfo obj1= new LanguageInfo();
+	obj1.tamilLanguage();
+	obj1.englishLanguage();
+	obj1.hindiLanguage();
+}
+}
