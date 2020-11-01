239dcbc75aca4da8b0830e328320161af0845b42
satzsaran <73731493+satzsaran@users.noreply.github.com>
2020-11-01 07:41:54 -0500

    Add files via upload

---
+++ GreensTech.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public class GreensTech {
+private void greensOmr() {
+	// TODO Auto-generated method stub
+System.out.println("omr 01");
+}
+private void greensAdayar() {
+	// TODO Auto-generated method stub
+System.out.println("adayar 02");
+}
+private void greensTambaram() {
+	// TODO Auto-generated method stub
+System.out.println("tam 03");
+}
+private void greensVelachery() {
+	// TODO Auto-generated method stub
+System.out.println("velachery 04");
+}
+private void greensAnnanagar() {
+	// TODO Auto-generated method stub
+System.out.println("annanagar 05");
+}
+public static void main(String[] args) {
+	GreensTech obj = new GreensTech();
+	obj.greensOmr();
+	obj.greensAdayar();
+	obj.greensTambaram();
+	obj.greensVelachery();
+	obj.greensAnnanagar();
+}
+}
---
+++ LanguageInfo.java
@@ -0,0 +1,17 @@
+package org.emp;
+
+public class LanguageInfo {
+	public  void tamilLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("tamil ");
+}
+	public  void hindiLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("hindi");
+}
+	public  void englishLanguage() {
+	// TODO Auto-generated method stub
+System.out.println("usa");
+}
+
+}
